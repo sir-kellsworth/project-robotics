1beca779016757b9ae146ae8ddb8798f5db964c1
sirkellsworth <>
2022-03-21 21:55:46 -0600

    should fix build issues anywhere

--- Arduino/RoboticArm/debug.sh
+++ Arduino/RoboticArm/debug.sh
@@ -6,7 +6,7 @@ pushd build
 cmake \
   -D ARDUINO_INSTALL_PATH=${HOME}/arduino-1.8.19 \
   -D ARDUINO_BOARD_OPTIONS_FILE=BoardOptions.cmake \
-  -D CMAKE_TOOLCHAIN_FILE=../../Arduino-CMake-Toolchain/Arduino-toolchain.cmake \
+  -D CMAKE_TOOLCHAIN_FILE=${HOME}/Arduino/Arduino-CMake-Toolchain/Arduino-toolchain.cmake \
   ..
 make -j $(nproc)
 
